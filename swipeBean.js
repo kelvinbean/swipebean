25e31fa867919ff7aa4713abbc8c29b36c7376de
Bean <260254788@qq.com>
2016-09-18 17:55:59 +0800

    update v.1.4
    v1.4  更新说明
    更新了部分功能。
    新增了一些对外接口，实现了一些图册效果。
    新增了overflow设置是否超出隐藏
    新增了回调函数接口
    新增了copyNum复制数量，来控制复制的对数
    修复了部分bug

--- swipeBean.js
+++ swipeBean.js
@@ -1,6 +1,6 @@
 
 /*
-	v 1.0.7
+	v 1.4
 
 	kelvinbean自制轮播插件
 
@@ -26,12 +26,20 @@
 		height:134,
 		// 包裹的父元素的query样式
 		list:'.swipe-list',
+		// 子元素Class
+		itemClass:'swipe-item',
+		// 是否自动播放
+		auto:true,
 		// 滑动方向
 		direction:'left',
+		// 是否超出隐藏
+		overflow:true,
 		// 间隔时间
 		iTime:2000,
 		// 滚动时间
 		duration:500,
+		// 自定义过渡效果
+		transition:'',
 		// 悬停是否停止切换（false表示继续切换，true表示停止切换）
 		hoverStop:false,
 		// 是否需要按钮
@@ -54,6 +62,10 @@
 		prevClass:'bean_prev',
 		// 当在移动端时，是否需要touch事件
 		isTouch:true,
+		// 回调函数（在图片切换的同时进行）
+		callback:null,
+		// 复制数量（一对为单位、一前一后）
+		copyNum:1,
 		// 以下几项无需自定义设置
 		// 延迟执行方法对象
 		timeobj:null,
@@ -77,9 +89,9 @@
 			case 'left':
 				option.bean = {
 					attrBean : 'left',//修改的属性/计算长度单位
-					initBean : '-'+option.width+'px',//初始为位置
+					initBean : '-'+(option.width*option.copyNum)+'px',//初始为位置
 					pageBean :  function(page){//根据页码移动的公式
-						return Number(-option.width*page)+'px';
+						return Number(-option.width*(page+option.copyNum-1))+'px';
 					},
 					translateBean : function(judge){//手机滑屏时对应的操作
 						if(judge < 0){
@@ -179,23 +191,33 @@
 		if(typeof(page) == 'undefined'){
 			page = option.pageNum;
 		}
-		if(page == option.childNum+1 && !isAuto){
-			page = 1;
-		}else if(page == 0){
-			page = option.childNum;
-		}
+
 		// 如果是当前页就不执行滑动
 		if(page != option.pageNum){
+			// 切换active类
+			itemActive(option,page);
 			// 执行滑动
 			option.swipeList.style[bean.attrBean] = bean.pageBean(page);
-
+			// 执行回调
+			if(typeof(option.callback) === 'function'){
+				option.callback(option);
+			}
 		}
-		// 判断是否是自动滑动
-		if(page == option.childNum+1 && isAuto){
+		// 判断是否需要重置
+		if(page == (option.childNum+1)){
 			setTimeout(function(){
-				option.swipeList.style.transition = 'none';
+				setTransition(option,false);
 				option.swipeList.style[bean.attrBean] = bean.initBean;
 				option.pageNum = 1;
+				if(option.hasBtn){
+					changeBtn(option,option.pageNum);
+				}
+			},option.duration);
+		}else if(page == 0){
+			setTimeout(function(){
+				setTransition(option,false);
+				option.swipeList.style[bean.attrBean] = bean.pageBean(option.childNum);
+				option.pageNum = option.childNum;
 				if(option.hasBtn){
 					changeBtn(option,option.pageNum);
 				}
@@ -207,12 +229,27 @@
 			}
 		}
 
-		// 自动滚到下一页
-		option.timeobj = setTimeout(function(){
-			option.swipeList.style.transition = (option.duration/1000) + 's all ease';
-			execMove(option,option.pageNum+1,true);
-		},option.iTime);
+		if(option.auto){
+			// 自动滚到下一页
+			option.timeobj = setTimeout(function(){
+				setTransition(option,true);
+				execMove(option,option.pageNum+1,true);
+			},option.iTime);
+		}
 
+	},
+
+	// 开启与关闭过渡效果(isOpen : true开，flase关)
+	setTransition = function(option,isOpen){
+		if(isOpen){
+			if(option.transition != ""){
+				option.swipeList.style.transition = option.transition;
+			}else{
+				option.swipeList.style.transition = (option.duration/1000) + 's all ease';
+			}
+		}else{
+			option.swipeList.style.transition = 'none';
+		}
 	},
 
 
@@ -254,6 +291,33 @@
 			}
 		}
 	},
+
+	// 子元素active的class切换
+	itemActive = function(option,page){
+
+		var children = option.swipeList.querySelectorAll('.'+option.itemClass);
+
+		var activeObj = option.swipeList.querySelectorAll('.'+option.itemClass+'.active');
+
+		for(var i=0;i<activeObj.length;i++){
+			activeObj[i].className = activeObj[i].className.replace(new RegExp('(^|\\b)\\s*'+'active'.split(' ').join('|') + '(\\b|$)', 'gi'), '');
+		}
+		
+		// 至少有两项才做这个active切换
+		if(option.childNum >= 2){
+			if(page > option.childNum){
+				page -= option.childNum;
+			}else if(page == 0){
+				page = option.childNum;
+			}
+			var toActiveObj = option.swipeList.querySelectorAll('.'+option.itemClass+'[data-page="'+page+'"]');
+			for(var i=0;i<toActiveObj.length;i++){
+				toActiveObj[i].className += ' active';
+			}
+		}else{
+			children[option.pageNum].className += ' active';
+		}
+	},
 	
 	// 设置容器元素样式
 	styleList = function(option){
@@ -262,21 +326,21 @@
 		switch (option.direction){
 			case 'left':
 				option.swipeList.style.top = '0px';
-				option.swipeList.style.left = '-'+option.width+'px';
-				option.swipeList.style.width = option.widthAll+(option.width*2)+'px';
+				option.swipeList.style.left = option.bean.initBean;
+				option.swipeList.style.width = option.widthAll+(option.width*(option.copyNum*2))+'px';
 				break;
 			case 'right':
 				option.swipeList.style.top = '0px';
-				option.swipeList.style.left = -option.widthAll+'px';
-				option.swipeList.style.width = option.widthAll+(option.width*2)+'px';
+				option.swipeList.style.left = option.bean.initBean;
+				option.swipeList.style.width = option.widthAll+(option.width*(option.copyNum*2))+'px';
 				break;
 			case 'top':
-				option.swipeList.style.top = '-'+option.height+'px';
+				option.swipeList.style.top = option.bean.initBean;
 				option.swipeList.style.left = '0px';
 				option.swipeList.style.width = option.width+'px';
 				break;
 			case 'bottom':
-				option.swipeList.style.top = -option.heightAll+'px';
+				option.swipeList.style.top = option.bean.initBean;
 				option.swipeList.style.left = '0px';
 				option.swipeList.style.width = option.width+'px';
 				break;
@@ -292,14 +356,16 @@
 		// 设置父元素样式
 		container.style.width = option.width+'px';
 		container.style.height = option.height+'px';
-		container.style.overflow = 'hidden';
+		// 是否超出隐藏
+		if(option.overflow){
+			container.style.overflow = 'hidden';
+		}
 		container.style.position = 'relative';
 
 		option.swipeList.outerHTML = container.outerHTML;
 
 		// 关键的一步,重新获取swipeList对象。因为上一步的outerHTML已经让swipeList对象变成container了。
 		option.swipeList = document.querySelector(option.list);
-
 		option.swipeList.parentNode.className = option.swipeList.className;
 		option.swipeList.className = '';
 
@@ -308,46 +374,55 @@
 	// 计算宽高并设置子元素样式
 	caculate = function(option){
 		// 计算宽度
-		var children = option.swipeList.childNodes;
-		var childNum = 0;
-		var firstChild = null;
-		var lastChild = null;
+		var children = option.swipeList.querySelectorAll('.'+option.itemClass);
 		var htmlArr = [];
+		var realChildArr = [];
 		for(var i=0;i<children.length;i++){
-			if(children[i].nodeType == 1){
-				childNum++;
-				if(firstChild === null){
-					firstChild = children[i];
-				}
-				// 设置子元素样式
-				children[i].style.float = option.direction=='right'?'right':'left';
-				children[i].style.width = option.width+'px';
-				children[i].style.height = option.height+'px';
-				lastChild = children[i];
+			if(i === 0){
+				children[i].className += ' active';
 			}
+			// 设置子元素样式
+			children[i].style.float = option.direction=='right'?'right':'left';
+			children[i].style.width = option.width+'px';
+			children[i].style.height = option.height+'px';
+			children[i].setAttribute("data-page",i+1);
+			realChildArr.push(children[i]);
 			htmlArr.push(children[i].outerHTML);
 		}
 
-		// 复制第一个属性到最后
-		option.swipeList.appendChild(firstChild.cloneNode(true));
-		// 复制最后一个属性到前面
-		option.swipeList.insertBefore(lastChild.cloneNode(true),firstChild);
+		if(option.direction != 'bottom'){
+			// 复制第一个属性到最后
+			option.swipeList.appendChild(realChildArr[0].cloneNode(true));
+			// 复制最后一个属性到前面
+			option.swipeList.insertBefore(realChildArr[realChildArr.length-1].cloneNode(true),realChildArr[0]);
 
-		// 计算总高度和总宽度
-		option.childNum = childNum;
-		option.widthAll = option.width * childNum;
-		option.heightAll = option.height * childNum;
+			// 处理需要复制多项的状况
+			if(option.copyNum > 1){
 
-		// 加入处理向下滑时，倒序输出
-		if(option.direction == 'bottom'){
-			htmlArr.push(firstChild.outerHTML);
+				var itemDom;
+
+				for(var i=1;i<option.copyNum;i++){
+					option.swipeList.appendChild(realChildArr[i].cloneNode(true));
+					itemDom = option.swipeList.querySelectorAll('.'+option.itemClass);
+					option.swipeList.insertBefore(realChildArr[children.length-1-i].cloneNode(true),itemDom[0]);
+				}
+			}
+		}else{
+			// 加入处理向下滑时，倒序输出
+			htmlArr.push(realChildArr[0].outerHTML);
+			htmlArr.unshift(realChildArr[realChildArr.length-1].outerHTML);
 			option.swipeList.innerHTML = htmlArr.reverse().join('');
 		}
+
+		// 计算总高度和总宽度
+		option.childNum = children.length;
+		option.widthAll = option.width * children.length;
+		option.heightAll = option.height * children.length;
 	},
 
 	// 绑定鼠标悬停停止滑动
 	pauseSwipe = function(option){
-		option.swipeList.onmousceenter = function(){
+		option.swipeList.onmouseenter = function(){
 			clearTimeout(option.timeobj);
 		};
 		option.swipeList.onmouseleave = function(){
@@ -358,37 +433,37 @@
 	// 绑定移动端滑动翻页事件
 	touchSwipe = function(option){
 		var startX,startY,nowX,nowY,x,y,initX,initY;
-
 		// 触屏操作
-		option.swipeList.ontouchstart = function(e){
+		option.swipeList.addEventListener('touchstart',function(e){
 			clearTimeout(option.timeobj);
 			startX = e.targetTouches[0].pageX;
 			startY = e.targetTouches[0].pageY;
 			initX = option.swipeList.offsetLeft;
 			initY = option.swipeList.offsetTop;
-		};
+		});
 
 		// 滑屏操作
-		option.swipeList.ontouchmove = function(e){
+		option.swipeList.addEventListener('touchmove',function(e){
 			nowX = e.targetTouches[0].pageX;
 			nowY = e.targetTouches[0].pageY;
 			x = nowX-startX;
 			y = nowY-startY;
 
 			if((option.direction == 'left' || option.direction == 'right') && Math.abs(x) < option.width){
-				e.preventDefault();
 				option.swipeList.style.transform = 'translateX('+x+'px)';
+				// option.swipeList.style.transform = 'translate3d('+x+',0,0)';
+				e.preventDefault();
 			}
 
 			if((option.direction == 'top' || option.direction == 'bottom') && Math.abs(y) < option.height){
-				e.preventDefault();
 				option.swipeList.style.transform = 'translateY('+y+'px)';
+				e.preventDefault();
 			}
 
-		};
+		});
 
 		// 手离开屏幕操作
-		option.swipeList.ontouchend = function(e){
+		option.swipeList.addEventListener('touchend',function(e){
 			if(option.direction == 'left' || option.direction == 'right'){
 				option.judge = x;
 				option.initVal = initX;
@@ -400,7 +475,7 @@
 				option.swipeList.style.transition = (option.duration/2000) + 's all ease';
 				option.swipeList.style.transform = option.bean.translateBean(option.judge);
 				setTimeout(function(){
-					option.swipeList.style.transition ='none';
+					setTransition(option,false);
 					option.swipeList.style.transform = 'translate(0,0)';
 					option.bean.touchBean(option.judge);
 				},option.duration/2);
@@ -410,7 +485,7 @@
 				option.swipeList.style[option.bean.attrBean] = option.initVal+'px';
 				execMove(option);
 			}
-		};
+		});
 	},
 
 	// 按钮拼接与绑定
@@ -441,7 +516,7 @@
 			var page;
 			if(e.target.nodeName.toLowerCase() == 'a'){
 				page = e.target.getAttribute('page');
-				option.swipeList.style.transition = (option.duration/1000) + 's all ease';
+				setTransition(option,true);
 				changeBtn(option,e.target);
 				execMove(option,Number(page));
 			}
@@ -455,11 +530,13 @@
 		if(type == 'prev'){
 			node.className = option.prevClass;
 			node.onclick = function(){
+				setTransition(option,true);
 				execMove(option,option.pageNum-1);
 			};
 		}else{
 			node.className = option.nextClass;
 			node.onclick = function(){
+				setTransition(option,true);
 				execMove(option,option.pageNum+1);
 			};
 		}
@@ -473,6 +550,9 @@
 
 		// 计算宽高并设置子元素样式
 		caculate(option);
+
+		// 配置滑动
+		doMove(option);
 
 		// 设置容器样式
 		styleList(option);
@@ -500,9 +580,6 @@
 			nextPrev(option,'prev');
 		}
 
-		// 配置滑动
-		doMove(option);
-
 		// 判断是否需要touch事件
 		if(option.isPhone && option.isTouch){
 			touchSwipe(option);
@@ -511,7 +588,6 @@
 		// 执行滑动
 		execMove(option,1,true);
 	};
-
 
 	// 初始化
 	swipe.init = function(options){
@@ -535,16 +611,24 @@
 
 		// 处理函数
 		deal(_option);
+		// 返回处理后的配置
+		return _option;
 		
 	};
 	// 下一页
 	swipe.next = function(option){
-		execMove(option,option.page+1);
+		setTransition(option,true);
+		execMove(option,option.pageNum+1);
 	};
 	// 上一页
 	swipe.prev = function(option){
-		execMove(option,option.page-1);
+		setTransition(option,true);
+		execMove(option,option.pageNum-1);
 	};
 
+	swipe.execMove = execMove;
+	swipe.setTransition = setTransition;
+
 	return swipe;
+
 });
